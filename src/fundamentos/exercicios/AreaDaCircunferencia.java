bc5634f97b7f8bb1d7ff4db9a0919492c8099783
Caetano <lcaetano30@gmail.com>
2021-12-24 15:56:03 -0300

    Circ Area

--- src/fundamentos/exercicios/AreaDaCircunferencia.java
+++ src/fundamentos/exercicios/AreaDaCircunferencia.java
@@ -4,7 +4,7 @@ public class AreaDaCircunferencia {
 
     public static void main(String[] args) {
 
-        double raio = 3.4;
+        double raio = 3.2;
         final double PI = 3.14159;
         double area =  PI * raio * raio;
         System.out.println("Área: " + area);
