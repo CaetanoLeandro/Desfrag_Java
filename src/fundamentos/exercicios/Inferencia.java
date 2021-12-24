cc7458b915b54497f744895a0dc39736ef29f8bd
Caetano <lcaetano30@gmail.com>
2021-12-24 16:12:14 -0300

    Exr Data inference

--- src/fundamentos/exercicios/Inferencia.java
+++ src/fundamentos/exercicios/Inferencia.java
@@ -4,7 +4,13 @@ public class Inferencia {
 
     public static void main(String[] args) {
 
+        double a = 4.5;
+        System.out.println(a);
 
+        var b = 5.4; // o Java define o tipo pelo valor inferido
+        System.out.println(b);
 
+        var c = "Texto";
+        System.out.println(c);
     }
 }
