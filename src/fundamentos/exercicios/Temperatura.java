4c35047f59a00daa1b9dea39d13f936558b2e56b
Caetano <lcaetano30@gmail.com>
2021-12-24 15:56:59 -0300

    Calc Temp

--- src/fundamentos/exercicios/Temperatura.java
+++ src/fundamentos/exercicios/Temperatura.java
@@ -9,7 +9,7 @@ public class Temperatura {
         double fahrenheit = 0.0;
         double celsius = 0.0;
         final int AJUSTE = 32;
-        final double FATOR = 5/9.0;
+        final double FATOR = 5.0/9.0;
 
         fahrenheit = 85;
         celsius = (fahrenheit - AJUSTE) * FATOR;
