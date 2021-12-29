8971f643e96c50c3ba116af6bda521d31247591b
Caetano <lcaetano30@gmail.com>
2021-12-29 09:47:25 -0300

    Condição ternária

---
+++ src/fundamentos/exercicios/CondTernaria.java
@@ -0,0 +1,12 @@
+package fundamentos.exercicios;
+
+public class CondTernaria {
+
+    public static void main(String[] args) {
+
+        double media = 7.4;
+        String resultadoParcial= media >= 5.0 ? "Em recuperação " : "Reprovado...";
+        String resultadoFinal = media >= 7.0 ? "Aprovado" : resultadoParcial;
+        System.out.println("O aluno está: " + resultadoFinal);
+    }
+}
