5500c4bf45acecee165f8fbf9913a4df02ced6a9
Caetano <lcaetano30@gmail.com>
2021-12-29 09:55:48 -0300

    Comparação de Strings

---
+++ src/fundamentos/exercicios/ComparacaoDeStrings.java
@@ -0,0 +1,12 @@
+package fundamentos.exercicios;
+
+public class ComparacaoDeStrings {
+
+    public static void main(String[] args) {
+
+        String s = new String("2");
+        System.out.println("2" == s); // False!
+        System.out.println("2".equals(s)); // True! Pois compara o conteúdo da variável
+
+    }
+}
